8f79d8155dc45e7c257c29fafb9d61c561667b8e
Dejan Cvijetinovic <dejancv9@gmail.com>
2019-05-27 18:24:42 +0200

    Add project files.

---
+++ BusinessTripAdvisor/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BusinessTripAdvisor.MvcApplication" Language="C#" %>
---
+++ BusinessTripAdvisor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BusinessTripAdvisor.Startup))]
+namespace BusinessTripAdvisor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
