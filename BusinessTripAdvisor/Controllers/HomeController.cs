99b38ec671788121533292dc93a131ea0a7261f0
Grupa 1 <dejancv9@gmail.com>
2019-06-11 11:09:29 +0200

    Added Transportation and feedbacks actions and views, partial views _NavBar and _Footer

--- BusinessTripAdvisor/Controllers/HomeController.cs
+++ BusinessTripAdvisor/Controllers/HomeController.cs
@@ -6,8 +6,10 @@ using System.Web.Mvc;
 
 namespace BusinessTripAdvisor.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
+        [AllowAnonymous]
         public ActionResult Index()
         {
             return View();
@@ -38,6 +40,16 @@ namespace BusinessTripAdvisor.Controllers
         }
 
         public ActionResult Accommodations()
+        {
+            return View();
+        }
+
+        public ActionResult Transportation()
+        {
+            return View();
+        }
+
+        public ActionResult Feedbacks()
         {
             return View();
         }
