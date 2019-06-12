dd1c438a1c94c9bce6a66fd76541e7198ff33117
Grupa 1 <dejancv9@gmail.com>
2019-06-12 12:13:26 +0200

    FIxed InfoCard sample view

--- BusinessTripAdvisor/FrontEnd/BTA/script.js
+++ BusinessTripAdvisor/FrontEnd/BTA/script.js
@@ -1,3 +1,4 @@
+
 const UI = (function () {
     const toSignUpForm = () => {
         document.querySelector("#sign-up-div").style.display = "block";
@@ -13,5 +14,24 @@ const UI = (function () {
 
     document.querySelector("#btn-to-sign-up").addEventListener("click", toSignUpForm);
     document.querySelector("#btn-to-log-in").addEventListener("click", toLogInForm);
+
 }) ();
 
+/*
+const UI = (function () {
+    const toSignUpForm = () => {
+        $("#sign-up-div").style.display = "block";
+        $("#log-in-div").style.display = "none";
+        $("title").textContent = "BTA | Sign up"
+    }
+
+    const toLogInForm = () => {
+        $("#log-in-div").style.display = "block";
+        $("#sign-up-div").style.display = "none";
+        $("title").textContent = "BTA | Log in"
+    }
+
+   $("#btn-to-sign-up").on("click", toSignUpForm);
+   $("#btn-to-log-in").on("click", toLogInForm);
+}) ();
+*/
