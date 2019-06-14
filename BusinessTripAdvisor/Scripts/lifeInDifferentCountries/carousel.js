122109f1c0e55d794d112e94bf158a09c31d3488
Grupa 1 <dejancv9@gmail.com>
2019-06-14 10:15:44 +0200

    LifeInDifferentCountries view separated in more files

---
+++ BusinessTripAdvisor/Scripts/lifeInDifferentCountries/carousel.js
@@ -0,0 +1,87 @@
+﻿// CAROUSEL ------------------CAROUSEL------------------------------CAROUSEL
+
+const track = document.querySelector('.carousel_track');
+const slides = Array.from(track.children);
+//console.log(slides);
+const nextButton = document.querySelector('.carousel_button--right');
+const prevButton = document.querySelector('.carousel_button--left');
+const dotsNav = document.querySelector('.carousel_nav');
+const dots = Array.from(dotsNav.children);
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+//console.log(slideWidth);
+
+//ARRANGE THE SLIDES NEXT TO ONE ANOTHER ----------------------------------
+const setSlidePosition = (slide, index) => {
+    slide.style.left = slideWidth * index + 'px';
+}
+slides.forEach(setSlidePosition);
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+}
+
+const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove('current-slide');
+    targetDot.classList.add('current-slide');
+}
+
+const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
+    if (targetIndex === 0) {
+        prevButton.classList.add('is-hidden');
+        nextButton.classList.remove('is-hidden');
+
+    } else if (targetIndex === slides.length - 1) {
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.add('is-hidden');
+    } else {
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    }
+}
+
+// *** Carousel functionality ***
+
+//WHEN I CLICK LEFT, MOVE SLIDE TO LEFT -------------------------------------
+prevButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const prevDot = currentDot.previousElementSibling;
+    const prevIndex = slides.findIndex(slide => slide === prevSlide);
+
+    moveToSlide(track, currentSlide, prevSlide);
+    updateDots(currentDot, prevDot);
+    hideShowArrows(slides, prevButton, nextButton, prevIndex);
+});
+
+// WHEN I CLICK RIGHT, MOVE SLIDE TO RIGHT------------------------------------
+nextButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const nextSlide = currentSlide.nextElementSibling;
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const nextDot = currentDot.nextElementSibling;
+    const nextIndex = slides.findIndex(slide => slide === nextSlide);
+
+    moveToSlide(track, currentSlide, nextSlide);
+    updateDots(currentDot, nextDot);
+    hideShowArrows(slides, prevButton, nextButton, nextIndex);
+});
+
+//WHEN I CLICK NAV INDICATOR, MOVE TO THAT SLIDE --------------------------------------
+dotsNav.addEventListener('click', e => {
+    //what indicator was clicked on
+    const targetDot = e.target.closest('button');
+
+    const currentSlide = track.querySelector('.current-slide');
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const targetIndex = dots.findIndex(dot => dot === targetDot);
+    const targetSlide = slides[targetIndex];
+
+    moveToSlide(track, currentSlide, targetSlide);
+    updateDots(currentDot, targetDot);
+    hideShowArrows(slides, prevButton, nextButton, targetIndex);
+
+});
---
+++ BusinessTripAdvisor/Scripts/lifeInDifferentCountries/mapApi.js
@@ -0,0 +1,187 @@
+﻿// *************************************************************
+// *** Life in different countries - Index - show all cities ***
+// *************************************************************
+
+var citiesInDb;
+
+$.ajax({
+    url: "/api/cities",
+    method: "GET",
+    success: function (responseData) {
+        citiesInDb = responseData;
+        if (responseData.length == 0)
+            $("#ponuda").append("<p>There are no cities in database.</p>");
+        else {
+            for (var i = 0; i < responseData.length; ++i) {
+                var city = responseData[i];
+
+                $("#ponuda").append(`<div id="card"><div class="row "><div class="picbox"><div class="pic"></div></div><div class="descriptionBox"><div class="card-block px-3"><h2 class="card-title">${city.name}</h2><p class="cardText">${city.description}</p><button class="btn btn-primary" onclick="details(${city.id})">Read More</button></div></div></div></div>`);
+                //$("#ponuda").append(`<div class="card" style="width: 35rem;"><img class="card-img-top" src="/FrontEnd/BTA/img/lisabon1.jpg" alt="Card image cap"><div class="card-body"><h5 class="card-title">${city.name}</h5><p class="card-text">${city.description}</p></div></div>`);
+            }
+        }
+    }
+});
+
+// *** Info card - carousel initialization ***
+
+// *** Create map object ***
+
+// Initialize the platform object:
+var platform = new H.service.Platform({
+    'app_id': 'CH8zl7pTYspa961FEXiT',
+    'app_code': 'bKCZBDWX2qKO0OPduRJIYA'
+});
+
+// Obtain the default map types from the platform object
+var maptypes = platform.createDefaultLayers();
+var mapContainer = document.getElementById('mapContainer');
+
+
+// Instantiate (and display) a map object:
+var map = new H.Map(
+    mapContainer,
+    maptypes.normal.map,
+    {
+        zoom: 10,
+        center: { lng: 13.4, lat: 52.51 }
+    }
+);
+
+// Define a variable holding SVG mark-up that defines an icon image:
+var svgMarkup = '<svg width="24" height="24" ' +
+    'xmlns="http://www.w3.org/2000/svg">' +
+    '<rect stroke="white" fill="#1b468d" x="1" y="1" width="22" ' +
+    'height="22" /><text x="12" y="18" font-size="12pt" ' +
+    'font-family="Arial" font-weight="bold" text-anchor="middle" ' +
+    'fill="white">H</text></svg>';
+// var svgMarkup = '<p>jdfhfjdsh</p>';
+// Create an icon, an object holding the latitude and longitude, and a marker:
+var icon = new H.map.Icon(svgMarkup),
+    coords = { lat: 52.53075, lng: 13.3851 };
+//marker = new H.map.Marker(coords, {icon: icon});
+
+// Add the marker to the map and center the map at the location of the marker:
+//map.addObject(marker);
+map.setCenter(coords);
+map.setZoom(12);
+
+//image upload
+//$('.file-upload').file_upload();
+
+/*-------------Mapa-----------------------------------*/
+
+var places;
+/*
+$(document).ready(function() {
+
+});
+*/
+
+// Load all matching cities in a list, add function to find and zoom chosen city in a map
+function loadPlacesList() {
+    var input = $("#inputPlace").val();
+    $("#placesList li.place").remove();
+    if (input.length > 2) {
+        $("#spinner").show();
+        $.ajax({
+            url: "http://autocomplete.geocoder.api.here.com/6.2/suggest.json",
+            data: {
+                app_id: "CH8zl7pTYspa961FEXiT",
+                app_code: "bKCZBDWX2qKO0OPduRJIYA",
+                query: input
+            },
+            method: "GET",
+            success: function (responseData) {
+                places = responseData.suggestions;
+                for (var placeId in places) {
+                    var place = places[placeId];
+                    if (place.matchLevel == "city")
+                        $("#placesList").append(`<li class="list-group-item list-group-item-action place" onclick="findOnMap('${place.locationId}')">${place.label}</li>`);
+                }
+                $("#spinner").hide();
+            },
+            error: function () {
+                $("#spinner").hide();
+                alert("Something went wrong.");
+            }
+        });
+    }
+}
+
+// Find city and zoom it in map using locationId
+function findOnMap(locationId) {
+    $.ajax({
+        url: "https://geocoder.api.here.com/6.2/geocode.json",
+        data: {
+            app_id: "CH8zl7pTYspa961FEXiT",
+            app_code: "bKCZBDWX2qKO0OPduRJIYA",
+            locationid: locationId
+        },
+        method: "GET",
+        success: function (responseData) {
+            var position = responseData.Response.View[0].Result[0].Location.DisplayPosition;
+            coords = {
+                lat: position.Latitude,
+                lng: position.Longitude
+            };
+            //var marker = new H.map.Marker(coords, {icon: icon});
+            //map.addObject(marker);
+            map.setCenter(coords);
+            map.setZoom(12);
+
+            var cityName = places.filter(place => place.locationId == locationId)[0].address.city;
+            //$("#test").text(cityName);
+            $("#inputPlace").val(cityName);
+            $("#placesList li.place").remove();
+        },
+        error: function () {
+            alert("Something went wrong.");
+        }
+    });
+}
+
+// Enable the event system on the map instance:
+var mapEvents = new H.mapevents.MapEvents(map);
+
+// *** Add/remove new marker ***
+
+// Add event listener:
+map.addEventListener('tap', function (evt) {
+    var geoCoords = toGeoCoords(map, evt.currentPointer.viewportX, evt.currentPointer.viewportY);
+
+    // Log 'tap' and 'mouse' events:
+    console.log({
+        type: evt.type,
+        currentPointer: evt.currentPointer.type,
+        viewportX: evt.currentPointer.viewportX,
+        viewportY: evt.currentPointer.viewportY,
+        viewBounds: map.getViewBounds(),
+        viewPort: map.getViewPort(),
+        geoCoords: geoCoords
+    });
+    // Create marker with fa - icon on click/tap
+    var domIcon = new H.map.DomIcon('<i class="fas fa-map-marker-alt fa-2x text-primary" style="top:-32px;left:-10px;"></i>');
+    var marker = new H.map.DomMarker(geoCoords, { icon: domIcon });
+    // Remove marker on longpress
+    marker.addEventListener('longpress', function (evt) {
+        map.removeObject(this);
+    });
+    map.addObject(marker);
+});
+
+// Instantiate the default behavior, providing the mapEvents object:
+var behavior = new H.mapevents.Behavior(mapEvents);
+
+// Converts viewport coordinates to geo coordinates
+function toGeoCoords(map, vX, vY) {
+    var viewBounds = map.getViewBounds();
+    var viewPort = map.getViewPort();
+    var geoHeight = Math.abs(viewBounds.ja - viewBounds.ka);
+    var geoWidth = Math.abs(viewBounds.ga - viewBounds.ha);
+    var ratioW = geoWidth / viewPort.width;
+    var ratioH = geoHeight / viewPort.height;
+    return {
+        lng: Math.min(viewBounds.ga, viewBounds.ha) + vX * ratioW,
+        lat: Math.max(viewBounds.ja, viewBounds.ka) - vY * ratioH
+    }
+}
